58c42eca9760048d4d8e8d67a0a08965834af2d2
TIP1 <tipthebaker@gmail.com>
2025-04-14 15:54:44 +0300

    rafactor of communication interface and implement singlton

--- agent/llm_interface.py
+++ agent/llm_interface.py
@@ -2,10 +2,31 @@
 
 import requests
 
-def query_model(prompt: str) -> str:
-    response = requests.post("http://localhost:11434/api/generate", json={
-        "model": "llama3.2",
-        "prompt": prompt,
-        "stream": False
-    })
-    return response.json().get("response", "")
+
+
+class ModelInterface:
+
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            return cls._instance
+
+    def __init__(self, model_name, url):
+        self.model_name = model_name
+        self.url = url
+
+        self.query_model(
+            f"My name is Tip. Text with me on russian language. Write responsec in json format. If you have a code, it must be in dictinary format where key is name of file with extension and content is value ")
+
+    def query_model(self, prompt: str) -> str:
+        print(f"Prompt: {prompt}")
+        response = requests.post(self.url, json={
+            "model": self.model_name,
+            "prompt": prompt,
+            "stream": False
+        })
+        answer = response.json().get("response", "")
+        print(f"Answer: {answer}")
+        return answer
--- ui/cli.py
+++ ui/cli.py
@@ -1,16 +1,19 @@
 # 🖥️ CLI (ui/cli.py):
-
+import time
 import typer
-from agent.llm_interface import query_model
+from agent.llm_interface import ModelInterface
 
 app = typer.Typer()
 
 @app.command()
 def chat():
+
+    llm_model = ModelInterface(model_name="mixtral", url="http://localhost:11434/api/generate")
+
     while True:
         prompt = input(">>> ")
         if prompt.lower() in ["exit", "quit"]:
             break
-        response = query_model(prompt)
+        response = llm_model.query_model(prompt)
         print("AI:", response)
 
